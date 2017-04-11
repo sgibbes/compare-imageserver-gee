b842f90efb7db17f2efce860ac0f9a10c428242d
mappingvermont <charlie.hofmann@gmail.com>
2017-04-11 13:37:33 -0400

    add pixel >> area_ha for imageserver responses

--- results/accuracy-plots.py
+++ results/accuracy-plots.py
@@ -2,27 +2,39 @@ import os
 import sys
 import json
 import pandas as pd
+import numpy as np
+
+import geodesic_area
 
 
 def main():
 
     df = load_data(sys.argv[1])
     zstats_df = load_zstats()
+    lat_area_dict = build_area_dict()
+    
+    print lat_area_dict
     
     melted_df = pd.melt(df, id_vars=['server_type', 'geojson_name'], var_name='year', value_name='loss_server')
     
     grouped_df = melted_df.groupby(['server_type', 'geojson_name', 'year'])['loss_server'].mean().reset_index()
     
+    grouped_df.to_csv('grouped.csv', index=None)
+    
+    # calculate esri area -- multiply the average area of the pixels at that latitude
+    # by the number of pixels in the esri response
+    grouped_df.ix[grouped_df.server_type == 'esri', 'loss_server'] = grouped_df['geojson_name'].map(lat_area_dict) * grouped_df['loss_server'] / 10000
+    
     joined_df = pd.merge(zstats_df, grouped_df, how='left', on=['geojson_name', 'year'])
     
-    joined_df['loss_zstats'] = pd.to_numeric(joined_df['loss_zstats'], errors='coerce')
+    #joined_df['loss_zstats'] = pd.to_numeric(joined_df['loss_zstats'], errors='coerce')
+    print joined_df
     
-    joined_df['pct_diff'] = ((joined_df['loss_server'] - joined_df['loss_zstats']) / joined_df['loss_zstats']) * 100
-    
-    print joined_df[(joined_df['server_type'] == 'gee') & (joined_df['geojson_name'] == 'brazil')]
+    joined_df['pct_diff'] = abs(((joined_df['loss_server'] - joined_df['loss_zstats'])) / joined_df['loss_zstats']) * 100
 
     final_df = joined_df.groupby(['server_type', 'geojson_name'])['pct_diff'].mean().reset_index()
     
+    final_df.to_csv('final.csv', index=False)
     print final_df
     
     
@@ -32,24 +44,47 @@ def load_data(datafile):
         
     output_list = []
     
+    # remove all ERROR responses
     data = [x for x in data if x['response'] != 'ERROR']
         
     for row in data:
         output_hist = [float(x) for x in row['response'].split(',')]
-        year_vals = range(2001, 2016)
+        year_vals = range(2001, 2015)
         
+        # remove "no loss" pixel values from esri response
+        if row['server_type'] == 'esri':
+            output_hist.pop(0)
+        
+        # create a dictionary of {year: loss_value} for each year
         row_dict = {year: val for (year, val) in zip(year_vals, output_hist)}
         row_dict['geojson_name'] = row['geojson_name']
         row_dict['server_type'] = row['server_type']
         
         output_list.append(row_dict)
         
+    # build a dataframe from the above, with each row being server_type, geojson, 2001, 2002, 2003, etc
     df = pd.DataFrame(output_list)    
     df['geojson_name'] = df['geojson_name'].apply(lambda x: os.path.splitext(os.path.basename(x))[0])
 
     return df
     
     
+def build_area_dict():
+
+    lat_dict = {'paraguay': -22.3355010247972,
+                'wdpa_idn_complex': -3.06276815170764,
+                'drc': -4.32550039144615}
+        
+    area_dict = {}
+        
+    for geojson_name, lat_val in lat_dict.iteritems():
+        pixel_area = geodesic_area.get_area(lat_val)
+        
+        area_dict[geojson_name] = pixel_area
+        
+    return area_dict
+    
+
 def load_zstats():
 
     zstats_df = pd.read_csv('arcgis_zstats_loss_results.csv')
